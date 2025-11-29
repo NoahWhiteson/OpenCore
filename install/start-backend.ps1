dc9ad494733d83c76e52f27063d43eecc67e6037
Noah <117399857+NoahWhiteson@users.noreply.github.com>
2025-11-29 13:28:21 -0500

    Delete install directory

--- install/start-backend.ps1
+++
@@ -1,8 +0,0 @@
-# Start OpenCore Backend Server
-
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location "$scriptPath\..\backend"
-
-Write-Host "Starting OpenCore Backend..." -ForegroundColor Green
-npm start
-
--- install/start-backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start OpenCore Backend Server
-
-cd "$(dirname "$0")/../backend" || exit 1
-
-echo "Starting OpenCore Backend..."
-npm start
-
